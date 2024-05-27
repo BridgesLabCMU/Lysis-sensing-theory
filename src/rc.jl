164b562340a91b8563c5fee33a33ad1a3b1805a4
TheHedgehog2000 <jprentic@andrew.cmu.edu>
2024-05-27 16:23:30 -0400

    Fixed bugs

--- src/rc.jl
+++ src/rc.jl
@@ -1,20 +1,19 @@
 using GLMakie
 using LinearAlgebra
-using Symbolics
 
 function main()
     # Define parameters
-    α = 1 
+    α = 10^(-10) 
     β = 100.0
-    η = 0.2
+    η = 0.05
     r = 1 
     K = 10^9
-    ν = 10^(-17) 
-    n = 2 
+    ν = 10^(-16) 
+    n =  2
 
     # Define the range for ω and κ
     ω_values = LinRange(0.0001, 1, 1000)
-    κ_values = LinRange(10^(-20), 10^(-19), 1000)
+    κ_values = LinRange(10^(-11), 10^(-8), 1000)
 
     # Preallocate a matrix for the function values
     f_values = Matrix{Float64}(undef, length(ω_values), length(κ_values))
@@ -32,7 +31,11 @@ function main()
             L_star = (ν * η / ω) * I_star
 
             # Calculate the function value
-            f = (ω + α * (L_star^n * V_star) / (κ^n + L_star^n)) / (1 - (N_star + I_star) / K)
+            f = (V_star * α * (1-L_star^n / (κ^n + L_star^n))) / (1 - (N_star + I_star) / K)
+            if f .< 0
+                @show V_star 
+            end
+            #f = (ω) / (1 - (N_star + I_star) / K)
             f_values[i, j] = f
         end
     end
@@ -40,8 +43,9 @@ function main()
     # Color the regions
     fig = Figure(size=(5*72,3*72))
     ax = Axis(fig[1, 1])
-    f_values_binary = f_values .< 0.7
-    hm = heatmap!(ax, ω_values, κ_values, f_values_binary, colormap = :binary, colorrange=(0,1))
+    f_values_binary = f_values .< 0.5
+    hm = heatmap!(ax, ω_values, κ_values, f_values, colormap = :seaborn_rocket_gradient)
+    Colorbar(fig[1, 2], hm)
     ax.xlabel = "Outflow rate, ω"
     ax.ylabel = "Half-max lysate, κ"
 
